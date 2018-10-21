f3bec8bb3afd92ef7bc35a9cc4989c59c7570fca
a <lol@mailinator.com>
2018-10-21 17:17:24 +1100

    save: rigol shit

--- utils/shuffle.py
+++ utils/shuffle.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python
 
+import sys
+import getopt
 import numpy as np
 
 if __name__ == "__main__":
-  print "fuck"
+  file = None
+  r,args = getopt.getopt(sys.argv[1:],"hf:",["help","file="])
+  for opt,arg in r:
+    if opt in ("-h","help"):
+      usage()
+      sys.exit(0)
+    elif opt in ("-f","--file"):
+      usage()
+      sys.exit(0)
