8ccccecbe8131c4f9ae1d205541271ab0e4c6bd4
lol <lol@lol.com>
2019-09-16 21:55:26 +1000

    changed fpga code

--- x/constr.xdc
+++ x/constr.xdc
@@ -1,11 +1,14 @@
 set_property IOSTANDARD LVCMOS33 [get_ports sc_clk]
 set_property IOSTANDARD LVCMOS33 [get_ports sc_io]
+set_property IOSTANDARD LVCMOS33 [get_ports sc_reset]
 set_property IOSTANDARD LVCMOS33 [get_ports trigger]
-set_property CLOCK_DEDICATED_ROUTE FALSE [get_nets sc_io_IBUF]
+set_property CLOCK_DEDICATED_ROUTE FALSE [get_nets sc_clk_IBUF]
 
 set_property IOSTANDARD LVCMOS33 [get_ports led_out]
+set_property IOSTANDARD LVCMOS33 [get_ports led_out_2]
 set_property PACKAGE_PIN H5 [get_ports led_out]
+set_property PACKAGE_PIN J5 [get_ports led_out_2]
 set_property PACKAGE_PIN G13 [get_ports sc_clk]
 set_property PACKAGE_PIN B11 [get_ports sc_io]
-set_property PACKAGE_PIN A11 [get_ports trigger]
-
+set_property PACKAGE_PIN A11 [get_ports sc_reset]
+set_property PACKAGE_PIN D12 [get_ports trigger]
--- x/glitch_trigger.v
+++ x/glitch_trigger.v
@@ -3,40 +3,58 @@
 module glitch_trigger(
     input sc_clk,
     input sc_io,
+    input sc_reset,
     output trigger,
-    output led_out
+    output led_out,
+    output led_out_2
     );
 
-reg [1:0] state;
-reg [16:0] ctr;
+// wire wait_io_edge;
+reg [1:0] wait_io_edge;
+reg [1:0] wait_clk_edge;
+reg [1:0] internal_io_edge;
+reg [1:0] internal_reset;
+reg [32:0] ctr_io;
+reg [32:0] ctr_clk;
 
-wire led_out;
+assign led_out = wait_clk_edge;
+assign led_out_2 = wait_io_edge;
+assign trigger = wait_clk_edge;
 
-assign led_out = state;
-assign trigger = state;
+parameter CLK_EDGE_TARGET = 13255;
+parameter IO_EDGE_TARGET = 720;
 
-parameter CTR_TARGET = 864;
-
-initial begin
-    state = 0;
-    ctr = 0;
-end
-
-always @(posedge sc_io)
+// MASTER CLOCK HERE.
+always @(posedge sc_clk)
 begin
-    if(sc_clk)
+    if (sc_reset == 0)
     begin
-        ctr <= ctr + 1;
-        if(ctr > CTR_TARGET)
-        begin
-            state <= 1;
-        end
-
+        wait_io_edge <= 0;
+        wait_clk_edge <= 0;
+        ctr_io <= 0;
+        ctr_clk <= 0;
+        internal_reset <= 0;
+        internal_io_edge <= 0;
     end
     else
     begin
-        state <= 0;
-        ctr <= 0;
+        if(sc_io == 1 && internal_io_edge == 0)
+        begin
+            ctr_io <= ctr_io + 1;
+            if(ctr_io > IO_EDGE_TARGET)
+            begin
+                wait_io_edge <= 1;
+            end
+        end
+        internal_io_edge <= sc_io;
+        if(wait_io_edge == 1 && wait_clk_edge == 0)
+        begin
+            ctr_clk <= ctr_clk + 1;
+        end
+        if(ctr_clk > CLK_EDGE_TARGET)
+        begin
+            wait_clk_edge <= 1;
+        end
     end
-end 
+end
 endmodule
